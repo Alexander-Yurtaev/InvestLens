3f924f6d2889c3ab91e45686cc66746c1dbf99b4
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-01-16 18:58:37 +0300

    Add UpdateIsRequiredMigration.

--- src/InvestLens.Data/InvestLens.Data.DataContext/Migrations/20260116155731_UpdateIsRequiredMigration.Designer.cs
+++ src/InvestLens.Data/InvestLens.Data.DataContext/Migrations/20260116155731_UpdateIsRequiredMigration.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using InvestLens.Data.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace InvestLens.Data.DataContext.Migrations
 {
     [DbContext(typeof(InvestLensDataContext))]
-    partial class InvestLensDataContextModelSnapshot : ModelSnapshot
+    [Migration("20260116155731_UpdateIsRequiredMigration")]
+    partial class UpdateIsRequiredMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
