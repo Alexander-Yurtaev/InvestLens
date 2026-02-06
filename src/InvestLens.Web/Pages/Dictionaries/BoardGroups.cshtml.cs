ca8b96dcbcef5984a42de82b72e577ce149f027c
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-02-06 08:20:34 +0300

    Implement BoardGroups. page.

---
+++ src/InvestLens.Web/Pages/Dictionaries/BoardGroups.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using InvestLens.Abstraction.Services;
+using InvestLens.Data.Entities.Index;
+using InvestLens.Web.Pages.Shared;
+
+namespace InvestLens.Web.Pages.Dictionaries;
+
+public class BoardGroupsModel : DictionaryBasePage<BoardGroup>
+{
+    public BoardGroupsModel(IBoardGroupDictionariesGrpcClientService service, ILogger<BoardGroupsModel> logger) : base(service, logger)
+    {
+    }
+
+    #region Protected Methods
+
+    public override string Route => "BoardGroups";
+
+    #endregion Protected Methods
+}
