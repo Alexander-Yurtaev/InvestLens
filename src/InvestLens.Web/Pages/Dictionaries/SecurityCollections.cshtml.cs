b5052330ba5d0331fcc70fbf47a8d6885f9ea196
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-02-06 10:53:32 +0300

    Implement SecurityCollections page.

---
+++ src/InvestLens.Web/Pages/Dictionaries/SecurityCollections.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using InvestLens.Abstraction.Services;
+using InvestLens.Data.Entities.Index;
+using InvestLens.Web.Pages.Shared;
+
+namespace InvestLens.Web.Pages.Dictionaries;
+
+public class SecurityCollectionsModel : DictionaryBasePage<SecurityCollection>
+{
+    public SecurityCollectionsModel(ISecurityCollectionDictionariesGrpcClientService service,
+        ILogger<SecurityCollectionsModel> logger) : base(service, logger)
+    {
+    }
+
+    #region Protected Methods
+
+    public override string Route => "SecurityCollections";
+
+    #endregion Protected Methods
+}
