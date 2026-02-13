6bb5f93f1b7c1ee907d76539796fda0433fd42a6
alexander.yurtaev <alexander.yurtaev@gmail.com>
2026-02-13 23:23:43 +0300

    Add RecreateIndexOnSecId migration.

---
+++ src/InvestLens.Data/InvestLens.Data.DataContext/Migrations/20260213201235_RecreateIndexOnSecId.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InvestLens.Data.DataContext.Migrations
+{
+    /// <inheritdoc />
+    public partial class RecreateIndexOnSecId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Securities_SecId_GIN",
+                table: "security");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_security_secid",
+                table: "security",
+                column: "secid",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_security_secid",
+                table: "security");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Securities_SecId_GIN",
+                table: "security",
+                column: "secid")
+                .Annotation("Npgsql:IndexMethod", "GIN")
+                .Annotation("Npgsql:IndexOperators", new[] { "gin_trgm_ops" });
+        }
+    }
+}
